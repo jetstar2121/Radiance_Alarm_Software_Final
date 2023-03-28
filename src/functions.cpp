98b76f10d054e3fdb7aa3abae25d5e379dc403f2
Patrick Hanley <patrickhanley2006@gmail.com>
2023-03-28 07:24:55 -0500

    Created new files: -"functions.cpp": For function definitions -"functions.h": For including function definitions -"includes.h":  For globally included files and code
    Modified Files:
    -"main.cpp": Moved functons and includes into appropriate new files

---
+++ src/functions.cpp
@@ -0,0 +1,14 @@
+// Functions File of Radiance_Alarm Software
+// Created by Patrick Hanley
+
+#include <includes.h>
+
+// Displays intro GIF
+unsigned long display_gif() {
+}
+
+// Enters home screen menu
+unsigned long home_screen() {
+
+  tft.fillScreen(MAGENTA);
+}
